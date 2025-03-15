9a3a71fc5154a421f63844672471dc5170c80fbb
Alexejewa-Uljana <aleexejewa.uljana@gmail.com>
2025-03-15 09:59:21 +0300

    Remove: CardUpgradeSystem.cpp

--- CardUpgradeSystem.cpp
+++
@@ -1,8 +0,0 @@
-#include "CardUpgradeSystem.h"
-#include <iostream>
-
-void CardUpgradeSystem::upgradeCard(Card* card) {
-    std::cout << "Upgrading ";
-    card->play();
-    std::cout << " Card upgraded!\n";
-}
