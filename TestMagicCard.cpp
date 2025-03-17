276eb350f0653298a5c591161d85f7c8c21b2f06
Alexejewa-Uljana <aleexejewa.uljana@gmail.com>
2025-03-17 19:24:50 +0300

    Add: initialization version

---
+++ TestMagicCard.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file TestMagicCard.cpp
+ * @brief Unit tests for the MagicCard class.
+ */
+
+#include "gtest/gtest.h"
+#include "MagicCard.h"
+#include "Player.h"
+
+/**
+ * @brief Tests the constructor of the MagicCard class.
+ * 
+ * Checks if the card is correctly initialized with a name and mana cost.
+ */
+TEST(MagicCard, Constructor) {
+    MagicCard magicCard(5, 5);
+    EXPECT_EQ(magicCard.getName(), "Magic Card");
+    EXPECT_EQ(magicCard.getManaCost(), 5);
+}
+
+/**
+ * @brief Tests playing a MagicCard.
+ * 
+ * The player's mana should decrease by the card's mana cost.
+ */
+TEST(MagicCard, Play) {
+    Player player;
+    MagicCard magicCard(5, 5);
+    player.reduceMana(magicCard.getManaCost());
+    EXPECT_EQ(player.getMana(), 5);
+}
+
+/**
+ * @brief Tests the retrieval of the mana cost of a MagicCard.
+ */
+TEST(MagicCard, ManaCost) {
+    MagicCard magicCard(5, 5);
+    EXPECT_EQ(magicCard.getManaCost(), 5);
+}
+
