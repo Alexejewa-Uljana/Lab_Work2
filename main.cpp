ef60fb9d5861b3b26665584c17ad449ce62fa33d
Alexejewa-Uljana <aleexejewa.uljana@gmail.com>
2025-04-16 22:47:33 +0300

    Add: main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include "Game.h"
+// Алексеева Ульяна st129990@student.spbu.ru
+#include "include/Game.h"
 #include <iostream>
 
 /**
  * @brief The entry point for the game.
  * Prompts the user to select a difficulty level and starts the game.
  */
-int main() {
+int main()
+{
     int difficulty;
     std::cout << "Choose difficulty (1 - easy, 2 - normal, 3 - boss fight): ";
     std::cin >> difficulty;
